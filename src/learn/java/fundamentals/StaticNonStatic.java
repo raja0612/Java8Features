ff264e032227bb2d7281c501fda4af2e45c102fd
raja0612 <rajashekhar.ramayampeta@gmail.com>
2016-11-19 10:12:59 -0500

    recalling all fundamentals

---
+++ src/learn/java/fundamentals/StaticNonStatic.java
@@ -0,0 +1,18 @@
+package learn.java.fundamentals;
+
+/**
+ * Created by RajashekharGoud on 11/19/16.
+ */
+public class StaticNonStatic {
+
+     public static int add(int a,int b){
+          System.out.println()
+          return a+b;
+     }
+
+     public static void main(String args[]){
+
+           System.out.println(StaticNonStatic.add(5,7));
+
+     }
+}
